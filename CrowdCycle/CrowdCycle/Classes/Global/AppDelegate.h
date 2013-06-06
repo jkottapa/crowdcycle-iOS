94ad8cf7bbd5b1634483d87d12cd1b859f79fc91
Daniel MacKenzie <danielmackenzie@v1406-wn-172-21-24-126.campus-dynamic.uwaterloo.ca>
2013-06-06 19:56:00 -0400

    Initial commit

---
+++ CrowdCycle/CrowdCycle/Classes/Global/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CrowdCycle
+//
+//  Created by Daniel MacKenzie on 2013-06-06.
+//  Copyright (c) 2013 Daniel MacKenzie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrowdCycle/CrowdCycle/Classes/UIViewControllers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CrowdCycle
+//
+//  Created by Daniel MacKenzie on 2013-06-06.
+//  Copyright (c) 2013 Daniel MacKenzie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
